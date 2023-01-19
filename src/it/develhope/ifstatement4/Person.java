4d689c9f3ce5e3c8cf8ba170069ce6d8cd6bb784
Giulia <giuliac35@gmail.com>
2023-01-19 18:20:13 +0100

    Create getLifeStage method

--- src/it/develhope/ifstatement4/Person.java
+++ src/it/develhope/ifstatement4/Person.java
@@ -9,4 +9,17 @@ public class Person {
         int rando = (int) Math.floor(Math.random()*(max-min+1)+min);
         age = rando;
     }
+
+    public String getLifeStage(){
+        if(age <= 12){
+            return "Child";
+        }else if(age >= 13 && age <=19){
+            return "Teen";
+        }else if(age >= 20 && age <=59){
+            return "Adult";
+        }else if(age >=60){
+            return "Senior adult";
+        }
+        return "Baby";
+    }
 }
