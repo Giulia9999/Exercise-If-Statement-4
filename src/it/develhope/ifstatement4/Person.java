e5fa3680047e38a3c2cb102145d6f307a6a91ee0
Giulia <giuliac35@gmail.com>
2023-01-19 18:14:37 +0100

    Create Person constructor

--- src/it/develhope/ifstatement4/Person.java
+++ src/it/develhope/ifstatement4/Person.java
@@ -2,4 +2,11 @@ package it.develhope.ifstatement4;
 
 public class Person {
     private int age;
+
+    public Person(){
+        int min = 5;
+        int max = 80;
+        int rando = (int) Math.floor(Math.random()*(max-min+1)+min);
+        age = rando;
+    }
 }
